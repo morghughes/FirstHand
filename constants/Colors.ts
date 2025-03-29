11498628f4769985fe00dc3e0e18314a349ae6fc
Justin Cresent <JCresent@hotmail.com>
2025-03-29 14:08:42 -0400

    Chnaging layout, added colors pallete, and working on flow

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,21 +3,21 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = '#C1121F';
+const tintColorDark = '#C1121F';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: '#FDF0D5',
+    background: '#669BBC',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: '#FDF0D5',
+    background: '#003049',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
