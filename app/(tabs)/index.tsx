c75b7edf66e2622d22b0dad6435c472e8febf020
Justin Cresent <JCresent@hotmail.com>
2025-03-29 15:08:42 -0400

    Working input fields for ios

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,34 +1,45 @@
-import { Image, StyleSheet, Platform, TextInput } from 'react-native';
+import { Image, StyleSheet, Platform, TouchableWithoutFeedback, Keyboard, TouchableOpacity, Text, View } from 'react-native';
+import { AutoGrowingTextInput } from 'react-native-autogrow-textinput';
 import React, { useState } from 'react';
 // import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
+const TAN = "#FDF0D5"
+const RED = '#C1121F'
+const DBLUE = '#003049'
+const LBLUE = '#669BBC'
+
+function handleSubmit() {
+
+}
+
 
 export default function HomeScreen() {
   const [text, setText] = useState('');
+  const [height, setHeight] = useState(40);
 
   return (
-    // <ParallaxScrollView
-    //   headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-    //   headerImage={
-    //     <Image
-    //       source={require('@/assets/images/partial-react-logo.png')}
-    //       style={styles.reactLogo}
-    //     />
-    //   }>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title" style={styles.centeredText}>Hello I am FirstHand, let me help assess the situation</ThemedText>
+        <ThemedText type="title" style={styles.centeredText}>FirstHand here, let me help assess the situation</ThemedText>
 
-        <TextInput
-          style={styles.input}
-          placeholder="Enter text here"
-          placeholderTextColor="#FDF0D5"
-          value={text}
-          onChangeText={text => setText(text)}
-        />
-    </ThemedView> 
-    //</ParallaxScrollView>
+        <View style={styles.inputContainer}>
+          <AutoGrowingTextInput 
+              style={styles.input}
+              placeholder="What is the situation?"
+              placeholderTextColor={TAN}
+              value={text}
+              onChangeText={(text: React.SetStateAction<string>) => setText(text)}
+              maxHeight={300}
+              minHeight={40}
+            />
+            <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+              <Text style={styles.buttonText}>Submit</Text>
+            </TouchableOpacity>
+          </View>
+      </ThemedView> 
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -53,11 +64,30 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
   },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '90%',
+    marginVertical: 15,
+  },
   input: {
+    flex: 0.8,
     margin: 15,
-    height: 40,
     borderColor: '#C1121F',
+    color: "#FDF0D5", 
     borderWidth: 1,
-    padding: 10
+    padding: 10,
+    borderRadius: 25,
+  },
+  button: {
+    flex: 0.2,
+    height: 40, 
+    backgroundColor: '#C1121F',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 25,
+  },
+  buttonText: {
+    color: 'FDF0D5'
   },
 });
